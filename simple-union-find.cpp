afc5cf9bff94b26d42ec4b2489ee3c77fa95419b
eukaryo <c3375c15@gmail.com>
2021-06-24 04:40:46 +0900

    Create simple-union-find.cpp

---
+++ simple-union-find.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<vector>
+
+class UnionFind {
+
+private:
+
+	std::vector<int64_t> v;
+	std::vector<int64_t> size;
+	int64_t tree_num;
+
+	int64_t find(int64_t a) {
+		if (v[a] == a)return a;
+		return v[a] = find(v[a]);
+	}
+
+public:
+
+	UnionFind(const int64_t N) :v(N), size(N) {
+		tree_num = N;
+        for (int64_t i = 0; i < N; ++i) {
+            v[i] = i;
+            size[i] = 1;
+        }
+	}
+
+	void unite(int64_t a, int64_t b) {
+		a = find(a);
+		b = find(b);
+		if (a == b)return;
+
+		if (size[a] < size[b])std::swap(a, b);
+		v[b] = a;
+		size[a] += size[b];
+		tree_num--;
+	}
+
+	bool same(int64_t a, int64_t b) { return find(a) == find(b); }
+
+	int get_size(int64_t a) {
+		return size[find(a)];
+	}
+	int get_tree_num() { return tree_num; }
+};
+
+int main() {
+
+    UnionFind uf(100);
+    std::cout << (uf.same(24, 57) ? "same" : "not same") << std::endl;
+    uf.unite(24, 57);
+    std::cout << (uf.same(24, 57) ? "same" : "not same") << std::endl;
+    std::cout << "size = " << uf.get_size(24) << std::endl;
+
+    return 0;
+}
